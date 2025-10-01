99c1fb4fdc73e1181bbc65e61fb0657f515fbd7d
Pedro Henrique Lima <pedroconicz@gmail.com>
2025-10-01 17:28:06 -0300

    Aprendendo sobre variáveis

---
+++ python_basico/variaveis.py
@@ -0,0 +1,9 @@
+# nome = 'Pedro'
+# idade = 19
+
+# print('Meu nome é',nome,'e eu tenho',idade,'anos')
+
+primeiro_num = 9
+segundo_num = 10
+resultado = primeiro_num + segundo_num
+print(resultado)
