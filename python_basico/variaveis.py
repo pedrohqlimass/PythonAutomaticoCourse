6792b6e91f4418915a4b88251659948bb0a9d023
Pedro Henrique Lima <pedroconicz@gmail.com>
2025-10-01 17:28:06 -0300

    Aprendendo sobre variáveis

--- python_basico/variaveis.py
+++ python_basico/variaveis.py
@@ -3,7 +3,16 @@
 
 # print('Meu nome é',nome,'e eu tenho',idade,'anos')
 
-primeiro_num = 9
-segundo_num = 10
-resultado = primeiro_num + segundo_num
-print(resultado)
+# primeiro_num = 9
+# segundo_num = 10
+# resultado = primeiro_num + segundo_num
+# print(resultado)
+
+# nome = input('Qual é o seu nome? ')
+# print(f'Prazer em te conhecer {nome}')
+
+primeiro_numero = int(input('Digite um número: '))
+segundo_numero = int(input('Digite outro número: '))
+resultado = primeiro_numero + segundo_numero
+
+print(f'A soma entre {primeiro_numero} e {segundo_numero} é {resultado}')
