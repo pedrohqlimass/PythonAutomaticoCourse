815d36860bd90f30d8d19f95d48a8aceda6a9702
Pedro Henrique Lima <pedroconicz@gmail.com>
2025-10-01 19:23:07 -0300

    Aprendendo sobre condicionais

---
+++ condicionais/if_else.py
@@ -0,0 +1,12 @@
+# idade = int(input('Digite sua idade: '))
+# if idade >= 18:
+#     print('Você é maior de idade')
+# else:
+#     print('Você é menor de idade')
+
+senha = 'batatinha'
+tentativa_senha = input('Digite sua senha: ')
+if tentativa_senha == senha:
+    print('Senha correta!')
+else:
+    print('Senha incorreta, tente novamente.')
